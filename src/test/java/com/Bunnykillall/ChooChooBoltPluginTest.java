9a759fbf5d040d6746dcded8e8af3b9a83e9f859
Bunnykillall <bunnykillallrs@gmail.com>
2025-04-02 22:55:02 +0100

    Choo Choo
    Signed-off-by: Scott Robinson <bunnykillallrs@gmail.com>

--- src/test/java/com/Bunnykillall/ChooChooBoltPluginTest.java
+++ src/test/java/com/Bunnykillall/ChooChooBoltPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.Bunnykillall;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ChooChooBoltPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ChooChooBoltPlugin.class);
 		RuneLite.main(args);
 	}
 }
